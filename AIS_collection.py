11064de5c2ccda6b207ae4141257445aea9d3190
AntoinePrs <43511451+AntoinePrs@users.noreply.github.com>
2019-05-27 18:56:21 +0200

    second commit

---
+++ AIS_collection.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Query AIShub data and store it locally
+
+It is necessary to have an AIShub API key and store it in a 
+text file in order to run it. 
+
+Author: Antoine Peris
+"""
+import requests
+import time
+import threading
+
+
+API_KEY = open("API_KEY.txt", "r").read()
+FORMAT="1"
+OUTPUT="csv"
+COMPRESS="2"
+
+url="http://data.aishub.net/ws.php?"+"username="+API_KEY+"&format="+FORMAT+"&output="+OUTPUT+"&compress="+COMPRESS
+r = requests.get(url)
+
+def get_ais():
+    threading.Timer(60, get_ais).start()
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%d%m%y_%H%M%S", named_tuple)
+    print("AIS "+time_string+ "downloaded")
+    name="ais_"+time_string+".csv.bzip2"
+    open(name, 'wb').write(r.content)
+
+get_ais()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AIS_db
+
+This script allow to retrieve data from AIShub every 30 minutes and store it locally. It can works only with an API key from AIShub.
+It is not possible to query more than once per minute. 
