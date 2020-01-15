79b2b4d7a4721bfbf04e0cfb411120ee2efd8aa3
Antoine Peris - BK <43511451+AntoinePrs@users.noreply.github.com>
2020-01-15 18:14:57 +0100

    use panda for csv handling

--- AIS_collection.py
+++ AIS_collection.py
@@ -7,26 +7,36 @@ text file in order to run it.
 
 Author: Antoine Peris
 """
-import requests
 import time
-import threading
+import os
+import pandas as pd
 
+
+os.getcwd()
 
 API_KEY = open("API_KEY.txt", "r").read()
 FORMAT="1"
 OUTPUT="csv"
-COMPRESS="2"
+COMPRESS="0"
+XMIN="2.329102"
+YMIN="49.389524"
+XMAX="7.767334"
+YMAX="53.859007"
 
-url="http://data.aishub.net/ws.php?"+"username="+API_KEY+"&format="+FORMAT+"&output="+OUTPUT+"&compress="+COMPRESS
 
+url="http://data.aishub.net/ws.php?"+"username="+API_KEY+"&format="+FORMAT+"&output="+OUTPUT+"&compress="+COMPRESS+"&latmin="+YMIN+"&latmax="+YMAX+"&lonmin="+XMIN+"&lonmax="+XMAX
 
 def get_ais():
-    threading.Timer(800, get_ais).start()
-    r = requests.get(url)
+    df = pd.read_csv(url)   
+    df=df[['TSTAMP','MMSI','NAVSTAT','SOG','COG','NAME','TYPE','LONGITUDE','LATITUDE']]
     named_tuple = time.localtime() # get struct_time
-    time_string = time.strftime("%d%m%y_%H%M%S", named_tuple)
-    print("AIS "+time_string+ "downloaded")
-    name="ais_"+time_string+".csv.bz2"
-    open(name, 'wb').write(r.content)
+    time_string = time.strftime("%Y%m%d_%H%M%S", named_tuple)
+    print("AIS "+time.strftime("%d/%m/%Y %H:%M:%S", named_tuple)+ " downloaded")
+    name="ais_"+time_string+".csv.gz"
+    df.to_csv(name, index = None, header=True,  compression='gzip')
 
-get_ais()
+    
+while True:
+    get_ais()
+    time.sleep(120)
+
